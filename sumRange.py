81e4ec1a5c611dc5c0635c72902c0ad7e2fb5c7c
Shreyans117 <shreyans.magdum@gmail.com>
2023-10-07 12:19:01 -0700

    Create sumRange.py

---
+++ sumRange.py
@@ -0,0 +1,21 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums=nums
+        self.prefix=[]
+        self.calcPrefix()
+    def calcPrefix(self):
+        self.prefix.append(self.nums[0])
+        for i in range(1,len(self.nums)):
+            self.prefix.append(self.nums[i]+self.prefix[i-1])
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left == 0:
+            return self.prefix[right]
+        else:
+            return self.prefix[right] - self.prefix[left - 1]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
