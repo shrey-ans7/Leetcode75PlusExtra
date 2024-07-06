d50164391dcd5a1a7bd51259a9dc88dd60137ee7
Shreyans117 <shreyans.magdum@gmail.com>
2024-07-06 00:03:25 -0700

    Update canCompleteCircuit.py
    Gas Station - Greedy (Removed Set)

--- canCompleteCircuit.py
+++ canCompleteCircuit.py
@@ -1,25 +1,21 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         last = len(gas)-1
-        visited = set()
         diffTot = 0
         i=0
-        visited=set()
         index=0
         while index<=last:
             diff = gas[i] - cost[i]
             diffTot += diff
-            visited.add(i)
             if diffTot<0:
                 diffTot=0
                 index+=1
                 i=index
-                visited = set()
                 continue
             i+=1
             if i>last:
                 i=0
-            if i in visited:
+            if i==index:
                 return index
         return -1
             
