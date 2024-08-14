6f55d2655ef9b4390081a6929683754dbfec6bc9
Shreyans117 <shreyans.magdum@gmail.com>
2024-08-13 18:25:11 -0700

    Create maxProduct.py
    152. Maximum Product Subarray
    Solved

---
+++ maxProduct.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        size = len(nums)
+        dp = {}
+        def dfs(i, currProd):
+            if (i, currProd) in dp:
+                return dp[(i,currProd)]
+            ans=currProd
+            if i+1<size:
+                ans=max(ans,dfs(i+1,currProd*nums[i+1]))
+                ans=max(ans,dfs(i+1,nums[i+1]))
+            dp[(i,currProd)]=ans
+            return ans
+        return dfs(0,nums[0])
+        
