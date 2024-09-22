eb680455d0d1e360384c12b4b8c4777d9e0ee267
Shreyans <shreyans.magdum@gmail.com>
2024-09-22 00:13:22 -0700

    Update maxProduct.py
    Changed recursion order to improve re-use.

--- maxProduct.py
+++ maxProduct.py
@@ -7,9 +7,9 @@ class Solution:
                 return dp[(i,currProd)]
             ans=currProd
             if i+1<size:
+                ans=max(ans,dfs(i+1,nums[i+1]))
                 if currProd!=0:
                     ans=max(ans,dfs(i+1,currProd*nums[i+1]))
-                ans=max(ans,dfs(i+1,nums[i+1]))
             dp[(i,currProd)]=ans
             return ans
         return dfs(0,nums[0])
