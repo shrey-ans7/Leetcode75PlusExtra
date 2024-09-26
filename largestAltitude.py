9715d0982eea585ca7250d2a261f6822bbf8afdd
Shreyans <shreyans.magdum@gmail.com>
2024-09-26 02:21:11 -0700

    Create largestAltitude.py

---
+++ largestAltitude.py
@@ -0,0 +1,11 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        size=len(gain)
+        prefix = [-1 for _ in range(size)]
+        prefix[0]=gain[0]
+        res=max(gain[0],0)
+        for i in range(1,size):
+            prefix[i]=gain[i]+prefix[i-1]
+            res=max(res,prefix[i])
+        return res
+        
