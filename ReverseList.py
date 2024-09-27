39fb4cabfa4039d654e3771a04314cd8f1b5c32f
Shreyans <shreyans.magdum@gmail.com>
2024-09-27 00:03:37 -0700

    Update ReverseList.py
    Added O(1) space solution.

--- ReverseList.py
+++ ReverseList.py
@@ -1,3 +1,27 @@
+#1. O(1) Space Complexity Solution.
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        newHead=None
+        if not head:
+            return head
+        def dfs(node):
+            nonlocal newHead
+            if node.next:
+                curr=dfs(node.next)
+                curr.next=node
+                return node
+            else:
+                newHead = node
+                return node
+        tail=dfs(head)
+        tail.next=None
+        return newHead
+#2. O(n) Space Complexity Solution.
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -19,3 +43,4 @@ class Solution:
             iter=iter.next
         iter.next=None
         return head
+
