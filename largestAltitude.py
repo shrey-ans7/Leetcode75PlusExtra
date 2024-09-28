3e0bc206a271c2881f120473bb23b2f9aa2074f0
Shreyans <shreyans.magdum@gmail.com>
2024-09-27 19:30:44 -0700

    Update largestAltitude.py
    1732. Find the Highest Altitude

--- largestAltitude.py
+++ largestAltitude.py
@@ -1,11 +1,22 @@
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
         size=len(gain)
-        prefix = [-1 for _ in range(size)]
-        prefix[0]=gain[0]
+        prefix=gain[0]
         res=max(gain[0],0)
         for i in range(1,size):
-            prefix[i]=gain[i]+prefix[i-1]
-            res=max(res,prefix[i])
+            prefix=gain[i]+prefix
+            res=max(res,prefix)
         return res
+        
+
+# class Solution:
+#     def largestAltitude(self, gain: List[int]) -> int:
+#         size=len(gain)
+#         prefix = [-1 for _ in range(size)]
+#         prefix[0]=gain[0]
+#         res=max(gain[0],0)
+#         for i in range(1,size):
+#             prefix[i]=gain[i]+prefix[i-1]
+#             res=max(res,prefix[i])
+#         return res
         
