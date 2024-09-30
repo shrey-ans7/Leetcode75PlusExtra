c3eaa7824a7cf58e061724b172abf9de8ce081b3
Shreyans <shreyans.magdum@gmail.com>
2024-09-30 00:23:40 -0700

    Update findKthLargest.py

--- findKthLargest.py
+++ findKthLargest.py
@@ -1,4 +1,6 @@
 #Explore Quickselect
+
+#Solution 1
 import heapq
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
@@ -6,4 +8,20 @@ class Solution:
         while len(nums)>k:
             res=heapq.heappop(nums)
         return nums[0]
+#Solution 2
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        kNums=[]
+        heapq.heapify(kNums)
+        count=0
+        for num in nums:
+            if count==k:
+                heapq.heappush(kNums,max(num,heapq.heappop(kNums)))
+            else:
+                heapq.heappush(kNums,num)
+                count+=1
+        return kNums[0]
+
+
         
