8bc65d347215e9d6d1c9390a73e5f877e2db1ab8
Shreyans <shreyans.magdum@gmail.com>
2024-10-02 01:42:24 -0700

    Update buildTreeFromInAndPre-Orders.py

--- buildTreeFromInAndPre-Orders.py
+++ buildTreeFromInAndPre-Orders.py
@@ -4,6 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# 1. Using indices
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        val_to_idx = {val:idx for idx,val in enumerate(inorder)}
+        size=len(preorder)
+        def dfs(preIndex, inStart, inEnd):
+            if not inorder or not preorder:
+                return None
+            if inStart>inEnd:
+                return None
+            node=TreeNode(preorder[preIndex])
+            mid=val_to_idx[preorder[preIndex]]
+            node.left=dfs(preIndex+1,inStart,mid-1)
+            node.right=dfs(preIndex+mid-inStart+1,mid+1,inEnd)
+            return node
+        return dfs(0,0,size-1)
+# 2.Using Sub-lists
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         high=len(preorder)
