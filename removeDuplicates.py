e02a5fa89de770442a58a344bd2ae08a33bf5a5c
Shreyans <shreyans.magdum@gmail.com>
2024-10-07 17:34:17 -0700

    Update removeDuplicates.py
    26. Remove Duplicates from Sorted Array

--- removeDuplicates.py
+++ removeDuplicates.py
@@ -1,3 +1,22 @@
+#1. Optimal Soln
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        prev=nums[0]-1
+        size=len(nums)
+        i=0
+        count=0
+        while i<size:
+            if nums[i]==prev:
+                i+=1
+                continue
+            else:
+                nums[count]=nums[i]
+                prev=nums[i]
+                count+=1
+                i+=1
+        return count
+
+#2. First Soln
 from collections import deque
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
