ec4661b80a21ec5e26f2da497aad02bbf2617386
Shreyans <shreyans.magdum@gmail.com>
2025-03-31 04:59:49 -0700

    Update findLUSlength.py

--- findLUSlength.py
+++ findLUSlength.py
@@ -1,18 +1,7 @@
 class Solution:
     def findLUSlength(self, a: str, b: str) -> int:
-        book={}
-        for i in range(len(a)):
-            book[a[:i+1]]=1
-        for i in range(len(b)):
-            if b[:i+1] not in book:
-                book[b[:i+1]]=1
-            else:
-                book[b[:i+1]]+=1
-        res=-1
-        for subseq, occ in book.items():
-            if occ==1:
-                res=max(res,len(subseq))
-        return res
+        return max(len(a),len(b)) if a!=b else -1
                 
+
 
         
