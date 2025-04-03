6f2c56809ceb1668caf2ac421dd6eb26835e3633
Shreyans <shreyans.magdum@gmail.com>
2025-04-02 20:56:23 -0700

    Update isBipartite.py

--- isBipartite.py
+++ isBipartite.py
@@ -1,28 +1,34 @@
 from collections import deque
-from typing import List
-
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        size = len(graph)
-        colors = [-1] * size
-
-        for i in range(size):
-            if colors[i] != -1:
-                continue  # Node already colored, skip it.
-            
-            # Start BFS for this component.
-            queue = deque([i])
-            colors[i] = 0  # Start coloring with 0.
-
-            while queue:
-                node = queue.popleft()
-                for neighbor in graph[node]:
-                    if colors[neighbor] == -1:
-                        # Color with the opposite color of `node`.
-                        colors[neighbor] = 1 - colors[node]
-                        queue.append(neighbor)
-                    elif colors[neighbor] == colors[node]:
-                        # If the neighbor has the same color, graph isn't bipartite.
+        n=len(graph)
+        visited=[-1 for _ in range(n)]
+        queue1=deque()
+        queue2=deque()
+        for index in range(n):
+            if visited[index]!=-1:
+                continue
+            queue1.append(index)
+            visited[index]=0
+            while(queue1):
+                node=queue1.pop()
+                color=visited[node]
+                for ngh in graph[node]:
+                    nghColor=visited[ngh]
+                    if nghColor==-1:
+                        visited[ngh]=1-color
+                    elif nghColor!=1-color:
                         return False
-
+                    else:
+                        continue
+                    queue2.append(ngh)
+                if not queue1:
+                    while(queue2):
+                        queue1.append(queue2.pop())
         return True
+
+
+
+
+
+        
