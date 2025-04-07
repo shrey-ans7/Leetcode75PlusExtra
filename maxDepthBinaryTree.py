b021e575acbbf9f4c5d04296d67697cd2cabac84
Shreyans <shreyans.magdum@gmail.com>
2025-04-07 01:39:56 -0700

    Update maxDepthBinaryTree.py

--- maxDepthBinaryTree.py
+++ maxDepthBinaryTree.py
@@ -6,7 +6,5 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if root==None:
-            return 0
-        return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
+        return max(self.maxDepth(root.left),self.maxDepth(root.right))+1 if root else 0
         
