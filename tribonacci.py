215253fc3877765fab6c51543e55ceeb3c3f2ad8
Shreyans <shreyans.magdum@gmail.com>
2025-04-08 01:13:06 -0700

    Update tribonacci.py

--- tribonacci.py
+++ tribonacci.py
@@ -1,16 +1,8 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        dp=[-1 for _ in range(n+1)] 
-        def dfs(i):
-            if i==0:
-                return 0
-            elif i==1 or i==2:
-                return 1
-            elif dp[i]!=-1:
-                return dp[i]
-            ans=dfs(i-3)+dfs(i-2)+dfs(i-1)
-            dp[i]=ans
-            return ans
-        return dfs(n)
-
-        
+        dp=[0,1,1] 
+        if n<3:
+            return dp[n]
+        for i in range(3,n+1):
+            dp[0], dp[1], dp[2] = dp[1], dp[2], sum(dp)
+        return dp[2]
