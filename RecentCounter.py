9285de96c46aa6f20d5956459a28be23c6563896
Shreyans <shreyans.magdum@gmail.com>
2025-04-10 01:46:36 -0700

    Create RecentCounter.py
    933. Number of Recent Calls

---
+++ RecentCounter.py
@@ -0,0 +1,24 @@
+from collections import deque
+class RecentCounter:
+
+    def __init__(self):
+        self.queue=deque()
+        
+
+    def ping(self, t: int) -> int:
+        count=1
+        for req in reversed(self.queue):
+            if req>=t-3000:
+                count+=1
+            else:
+                break
+        self.queue.append(t)
+        return count
+
+
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
