8c0813cfe96165e7e24ce2a4f18b9e90c0eb3c17
Shreyans <shreyans.magdum@gmail.com>
2025-04-10 23:53:46 -0700

    Update pairSum.java

--- pairSum.java
+++ pairSum.java
@@ -13,17 +13,22 @@ class Solution {
     public int pairSum(ListNode head) {
         Stack<Integer> twins = new Stack<>();
         ListNode iter = head;
+        int size=0;
         while(iter!=null) {
             twins.push(iter.val);
             iter=iter.next;
+            size+=1;
         }
         iter=head;
         int res=Integer.MIN_VALUE;
         int twin=Integer.MIN_VALUE;
+        int count=0;
         while(!twins.isEmpty()){
             twin=twins.pop();
             res=Math.max(res,twin+iter.val);
             iter=iter.next;
+            count+=1;
+            if (count==size/2) break;
         }
         return res;
         
