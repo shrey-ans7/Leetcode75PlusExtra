6a64d513e2fb2f36d4195d0b585d8b0183d8e794
Shreyans <shreyans.magdum@gmail.com>
2025-04-13 16:11:28 -0700

    Update lengthOfLISRecursive.py

--- lengthOfLISRecursive.py
+++ lengthOfLISRecursive.py
@@ -1,3 +1,29 @@
+#1. Optimal Soln:
+#TODO
+
+#2. Accepted O(n^2) Memory and O(n^2) Time Soln:
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        size=len(nums)
+        dp=[[-1 for _ in range(size+1)] for _ in range(size+1)]
+        nums.append(-sys.maxsize)
+        def dfs(i,j):
+            if i>=size:
+                return 0
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            #1. Skip
+            ans1=dfs(i+1,j)
+            ans2=-1
+            if nums[j]<nums[i]:
+                ans2=1+dfs(i+1,i)
+            ans2=max(ans1,ans2)
+            dp[i][j]=ans2
+            return ans2
+            
+        return dfs(0,size)
+        
+#3. Least Efficient Soln:
 from typing import List, Dict
 import sys
 
