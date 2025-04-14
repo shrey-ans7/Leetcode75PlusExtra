f347ecb49f6eb7945b22e352282d5b266a56b6e2
Shreyans <shreyans.magdum@gmail.com>
2025-04-13 22:29:44 -0700

    Update moveZeroes.py
    283. Move Zeroes

--- moveZeroes.py
+++ moveZeroes.py
@@ -1,3 +1,18 @@
+#Soln 1:
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        size=0
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[size]=nums[i]
+                size+=1
+        for i in range(size,len(nums)):
+            nums[i]=0
+        return
+#Soln 2:
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
