7a6ad6ea0a6dac7d8c00425d163b6874776c05c9
Shreyans <shreyans.magdum@gmail.com>
2025-04-20 18:35:10 -0700

    Update findMinArrowShots.py
    452. Minimum Number of Arrows to Burst Balloons

--- findMinArrowShots.py
+++ findMinArrowShots.py
@@ -1,6 +1,6 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort(key=lambda point:(point[0],-point[1]))
+        points.sort(key=lambda point:point[0])
         prev=points[0][1]
         size=len(points)
         count=1
