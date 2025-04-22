b97adde0b2003ad804ad2d4f3950731839c01eec
Shreyans <shreyans.magdum@gmail.com>
2025-04-21 21:22:54 -0700

    Update binarySearchMatrix.py
    74. Search a 2D Matrix

--- binarySearchMatrix.py
+++ binarySearchMatrix.py
@@ -1,40 +1,32 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        def binarySearchL1(matrix, target):
-            def search(nums, target):
-                low=0
-                high=len(nums)
-                mid = (low+high)//2
-                print(nums, low, high, mid)
-                if low==high:
-                    return -1
-                if nums[mid]==target:
-                    return mid
-                elif target<nums[mid]:
-                    return search(nums[0:mid], target)
-                else:
-                    tmp=search(nums[mid+1:high], target)
-                    print(tmp)
-                    if(tmp==-1):
-                        return -1
-                    return mid+1+tmp
+        low=0
+        high=len(matrix)-1
+        def search(row):
             low=0
-            high=len(matrix)
-            mid=low+high//2
-            if low==high-1:
-                return search(matrix[low],target)
-            elif matrix[mid][0]==target:
-                return 0
-            elif matrix[mid][0]>target:
-                return binarySearchL1(matrix[0:mid],target)
-            else:
-                if mid+1==high or matrix[mid+1][0]>target:
-                    return search(matrix[mid], target)
-                tmp=binarySearchL1(matrix[mid+1:high], target)
-                if(tmp==-1):
-                    return -1
-                return mid+1+tmp
-        ans=binarySearchL1(matrix, target)
-        if ans==-1:
+            high=len(row)-1
+            while low<=high:
+                mid=low+(high-low)//2
+                if row[mid]==target:
+                    return True
+                elif row[mid]>target:
+                    high=mid-1
+                else:
+                    low=mid+1
             return False
-        return True
+
+
+        while low<=high:
+            mid=low+(high-low)//2
+            if matrix[mid][0]==target:
+                return True
+            elif matrix[mid][0]>target:
+                high=mid-1
+            else:
+                if mid+1>high or matrix[mid+1][0]>target:
+                    return search(matrix[mid])
+                else:
+                    low=mid+1
+        return False
+
+        
