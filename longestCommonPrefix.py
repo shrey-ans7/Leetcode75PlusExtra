38b9b75164ae8ad620f50879d48f7fcf557b3569
Shreyans <shreyans.magdum@gmail.com>
2025-06-04 13:01:40 -0700

    Update longestCommonPrefix.py

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -12,3 +12,32 @@ class Solution:
         return strs[0][0:i]
         
         
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+        curr_range=len(strs[0])
+        chars={}
+        count=0
+        for char in strs[0]:
+            chars[count]=char
+            count+=1
+        for i in range(1,len(strs)):
+            curr_range=min(curr_range,len(strs[i]))
+            for j in range(len(strs[i])):
+                if j not in chars:
+                    curr_range=min(curr_range,j)
+                    break
+                if j==curr_range:
+                    break
+                if chars[j]==strs[i][j]:
+                    continue
+                else:
+                    curr_range=min(curr_range,j)
+                    break
+        return strs[0][:curr_range]
+
+
+                
+        
+        
