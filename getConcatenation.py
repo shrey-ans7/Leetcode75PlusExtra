9aa35337f564456896e61c8cff188b8d2510d0ab
Shreyans <shreyans.magdum@gmail.com>
2025-06-06 12:43:40 -0700

    Update getConcatenation.py
    Using mod

--- getConcatenation.py
+++ getConcatenation.py
@@ -1,5 +1,11 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans=[]
+        for i in range(2*len(nums)):
+            ans.append(nums[i%len(nums)])
+        return ans
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
         n=len(nums)
         ans = [0 for i in range(2*n)]
         for i in range(n):
