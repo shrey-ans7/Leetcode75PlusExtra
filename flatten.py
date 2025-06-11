e53df649f5d08f4074a57dac12eb006d8e3bc20e
Shreyans <shreyans.magdum@gmail.com>
2025-06-11 15:18:17 -0700

    Update flatten.py
    114. Flatten Binary Tree to Linked List

--- flatten.py
+++ flatten.py
@@ -5,6 +5,33 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        prev=None
+        def dfs(root):
+            nonlocal prev
+            if not root:
+                return
+            dfs(root.right)
+            dfs(root.left)
+            root.right=prev
+            root.left=None
+            prev=root
+            return 
+        dfs(root)
+        return root
+
+#Soln 2:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 from collections import deque
 class Solution:
     def flatten(self, root: Optional[TreeNode]) -> None:
