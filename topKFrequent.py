9477ad3bf6ead6c82fef7e2d11ebed9c2bbc85fd
Shreyans <shreyans.magdum@gmail.com>
2025-06-25 12:01:31 -0700

    Update topKFrequent.py

--- topKFrequent.py
+++ topKFrequent.py
@@ -1,18 +1,15 @@
-import heapq
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        holder=[]
         tracker={}
-        ans=[]
-        for i in nums:
-            if i not in tracker.keys():
-                tracker[i]=1
-            else:
-                tracker[i]+=1
+        for num in nums:
+            tracker[num]=tracker.get(num,0)+1
+        import heapq
+        queue=[]
+        for key, value in tracker.items():
+            heapq.heappush(queue,(value, key))
+            if len(queue)>k:
+                heapq.heappop(queue)
+        return [key for val, key in queue]
 
-        for i,j in tracker.items():
-            holder.append([i,j])
-        holder.sort(key=lambda x:-x[1])
-        for i in range(k):
-            ans.append(holder[i][0])
-        return ans
+
+        
