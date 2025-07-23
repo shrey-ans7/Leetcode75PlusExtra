ccacca96adb3b51f7fa0b3e5d69deb9610bf09c0
Shreyans <22623133+ShkadovThruster@users.noreply.github.com>
2025-07-23 14:34:43 -0700

    Update reorderList.py
    143. Reorder List
    
    Signed-off-by: Shreyans <22623133+ShkadovThruster@users.noreply.github.com>

--- reorderList.py
+++ reorderList.py
@@ -1,3 +1,38 @@
+#Soln 1 (New)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        fast=head
+        slow=head
+        while fast.next:
+            slow=slow.next
+            fast=fast.next.next if fast.next.next else fast.next
+        prev=None
+        itr=slow.next
+        slow.next=None
+        while itr:
+            nxt=itr.next
+            itr.next=prev
+            prev=itr
+            itr=nxt
+        itr1=head
+        itr2=prev
+        while itr2:
+            nxt1=itr1.next
+            nxt2=itr2.next
+            itr1.next=itr2
+            itr2.next=nxt1
+            itr1.next=itr2
+            itr1=nxt1
+            itr2=nxt2
+        return 
+
+#Soln 2 (Old)        
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
