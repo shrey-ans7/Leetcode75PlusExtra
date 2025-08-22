842d02c4ce96158ae7c7ecbd04e063e6b3feb4c0
Shreyans <22623133+ShkadovThruster@users.noreply.github.com>
2025-08-22 14:36:56 -0700

    Update findOrder.py
    210. Course Schedule II
    
    Signed-off-by: Shreyans <22623133+ShkadovThruster@users.noreply.github.com>

--- findOrder.py
+++ findOrder.py
@@ -1,3 +1,4 @@
+##Soln 1: DFS with Memo
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph={}
@@ -33,6 +34,34 @@ class Solution:
             ans.reverse()
             return ans
         return []
+        
+#Soln 2: Topological Sort
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[bool]:
+        graph={}
+        in_degree=[0 for _ in range(numCourses)]
+        for course in range(numCourses):
+            graph[course]=[]
+        for prereq in prerequisites:
+            in_degree[prereq[0]]+=1
+            graph[prereq[1]].append(prereq[0])
+        queue=[]
+        for crs in range(numCourses):
+            if in_degree[crs]==0:
+                queue.append(crs)
+        index=0
+        res=[]
+        while index<len(queue):
+            for crs in graph[queue[index]]:
+                in_degree[crs]-=1
+                if in_degree[crs]==0:
+                    queue.append(crs)
+            index+=1
+        return queue if len(queue)==numCourses else []
+
+
+        
+
 
 
         
