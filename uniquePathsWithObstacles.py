3ae81bd42c4efa586036bddeef70ca1824afa70f
Shreyans <22623133+ShkadovThruster@users.noreply.github.com>
2025-08-26 12:42:50 -0700

    Update uniquePathsWithObstacles.py
    Signed-off-by: Shreyans <22623133+ShkadovThruster@users.noreply.github.com>

--- uniquePathsWithObstacles.py
+++ uniquePathsWithObstacles.py
@@ -1,3 +1,32 @@
+#Soln 1:
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0] * n for _ in range(m)]
+
+        # Start cell
+        if obstacleGrid[0][0] == 1:
+            return 0
+        dp[0][0] = 1
+
+        # First column
+        for i in range(1, m):
+            if obstacleGrid[i][0] == 0 and dp[i-1][0] == 1:
+                dp[i][0] = 1
+
+        # First row
+        for j in range(1, n):
+            if obstacleGrid[0][j] == 0 and dp[0][j-1] == 1:
+                dp[0][j] = 1
+
+        # Fill rest of dp
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 0:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        return dp[m-1][n-1]
+#Soln 2:
 class Solution:
     dp=None
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
