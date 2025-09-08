c71e74cbf34e38b2008853243a52bc74567b91d4
Shreyans <22623133+shrey-ans7@users.noreply.github.com>
2025-09-08 12:22:03 -0700

    Update reverse.py
    7. Reverse Integer
    
    Signed-off-by: Shreyans <22623133+shrey-ans7@users.noreply.github.com>

--- reverse.py
+++ reverse.py
@@ -1,4 +1,5 @@
 
+#Soln 1:
 class Solution:
     INT_MIN, INT_MAX = -2**31, 2**31 - 1
     def reverse(self, x: int) -> int:
@@ -11,6 +12,19 @@ class Solution:
             seed*=10
             seed+=x%10
             x=x//10
+        if seed > self.INT_MAX or seed <self.INT_MIN:
+            return 0
+        return -seed if is_neg else seed
+        
+#Soln 2:
+
+class Solution:
+    INT_MIN, INT_MAX = -2**31, 2**31 - 1
+    def reverse(self, x: int) -> int:
+        is_neg=True if x<0 else False
+        if is_neg:
+            x*=-1
+        seed=(int(str(x)[::-1]))
         if seed > self.INT_MAX or seed <self.INT_MIN:
             return 0
         return -seed if is_neg else seed
