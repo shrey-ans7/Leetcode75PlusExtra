6469614b20044c49c3371958c30ff9e2c4a3c873
Shreyans <22623133+shrey-ans7@users.noreply.github.com>
2025-09-08 11:46:34 -0700

    Create reverse.py
    7. Reverse Integer
    
    Signed-off-by: Shreyans <22623133+shrey-ans7@users.noreply.github.com>

---
+++ reverse.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    INT_MIN, INT_MAX = -2**31, 2**31 - 1
+    def reverse(self, x: int) -> int:
+        is_neg=True if x<0 else False
+        if is_neg:
+            x*=-1
+        seed=0
+        count=0
+        while x:
+            seed*=10
+            seed+=x%10
+            x=x//10
+        if seed > self.INT_MAX or seed <self.INT_MIN:
+            return 0
+        return -seed if is_neg else seed
+        
